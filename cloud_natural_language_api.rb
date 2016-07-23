53bb44484c1ca7b2f6cab55570e47f24f465faa3
taise <taise515@gmail.com>
2016-07-24 00:22:48 +0900

    Add: Host name

--- cloud_natural_language_api.rb
+++ cloud_natural_language_api.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class CloudNaturalLanguageAPI
+  HOST = 'language.googleapis.com'
+
   attr_accessor :api_key
   def initialize(api_key)
     self.api_key = api_key
--- test/test_cloud_natural_language_api.rb
+++ test/test_cloud_natural_language_api.rb
@@ -1,14 +1,19 @@
 # frozen_string_literal: true
 require 'test/unit'
-require 'test/unit/notify'
-
 require_relative '../cloud_natural_language_api'
 
 class TestCloudNaturalLanguageAPI < Test::Unit::TestCase
   API_KEY = 'api_key'
 
+  def setup
+    @cnl_api = CloudNaturalLanguageAPI.new(API_KEY)
+  end
+
   def test_new
-    cnl_api = CloudNaturalLanguageAPI.new(API_KEY)
-    assert(API_KEY, cnl_api.api_key)
+    assert(API_KEY, @cnl_api.api_key)
+  end
+
+  def test_HOST
+    assert('language.googleapis.com', CloudNaturalLanguageAPI::HOST)
   end
 end
