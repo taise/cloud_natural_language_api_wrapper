047bdd235d08720597ea4110f6ae9a45517e19c9
taise <taise515@gmail.com>
2016-07-24 21:14:26 +0900

    Add: exsample for analyze sentiment (en)

---
+++ exsample/analyze_sentiment_en.rb
@@ -0,0 +1,24 @@
+require_relative '../cloud_natural_language_api'
+
+
+CONTENT = <<-'EOF'
+Alice was beginning to get very tired of sitting by her sister on the bank,
+and of having nothing to do: once or twice she had peeped into the book her
+sister was reading, but it had no pictures or conversations in it,
+'and what is the use of a book,' thought Alice 'without pictures or conversations?'
+EOF
+# from: Alice's Adventures in Wonderland / Lewis Carroll
+# http://www.gutenberg.org/files/11/11-h/11-h.htm
+
+cnl = CloudNaturalLanguageAPI.new(ENV['API_KEY'])
+puts cnl.analyze_sentiment(CONTENT)
+
+=begin
+{
+  "documentSentiment": {
+    "polarity": -1,
+    "magnitude": 0.9
+  },
+  "language": "en"
+}
+=end
